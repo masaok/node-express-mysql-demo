2094eaa1cb155535056d765c9233e5f0867aca05
Masao Kitamura <masaok@gmail.com>
2020-11-23 04:08:02 -0800

    /customers endpoint now works via Postman

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,22 @@
-const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+const mysql = require('mysql2')
+const dbConfig = require('../config/db.config.js')
 
-var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+require('dotenv').config()
 
-module.exports = connection;
+// var connection = mysql.createPool({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+// });
+
+const dbh = mysql.createConnection({
+  host: process.env.DBHOST,
+  port: 3306,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBUSER,
+})
+
+module.exports = dbh
+// export default dbh
